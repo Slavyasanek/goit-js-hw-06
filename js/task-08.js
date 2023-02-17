07420ada4807bfaf011b3281ef39e9162f49aa92
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-02-17 22:03:11 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+"use strict";
 
+const loginForm = document.querySelector('.login-form');
+
+const checkValidation = () => {
+    event.preventDefault();
+    const {elements: 
+        { email, password }
+    } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        return alert(`All fields must be completed!`);
+    }
+    if (email.value && password.value) {
+        console.log(`Email is: ${email.value}. Password is: ${password.value}`);
+    }
+    event.currentTarget.reset();
+}
+
+loginForm.addEventListener('submit', checkValidation);
