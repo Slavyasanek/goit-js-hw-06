4087dd56b00d985c6b77cc476c738b6ea2d8e824
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-02-17 22:10:27 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+"use strict";
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const btnChangeColor = document.querySelector('.change-color');
+const colorOutput = document.querySelector('.color')
+
+const changeBcg = () => {
+  let currentColor = getRandomHexColor();
+  body.style.backgroundColor = currentColor;
+  colorOutput.textContent = currentColor;
+}
+
+btnChangeColor.addEventListener("click", changeBcg)
