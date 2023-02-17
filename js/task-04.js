fe5613990db8cb497edc5ffe523c4c6bec5cc3af
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-02-17 20:35:49 +0200

    task4

---
+++ js/task-04.js
@@ -0,0 +1,17 @@
+const decrBtn = document.querySelector('[data-action="decrement"]');
+const incBtn = document.querySelector('[data-action="increment"]');
+const valueOutput = document.querySelector('#value');
+let counterValue = 0;
+
+const decrease = () => {
+    counterValue-=1; 
+    valueOutput.textContent = counterValue;
+}
+
+const increase = () => {
+    counterValue +=1;
+    valueOutput.textContent = counterValue;
+}
+
+decrBtn.addEventListener("click", decrease);
+incBtn.addEventListener("click", increase)
