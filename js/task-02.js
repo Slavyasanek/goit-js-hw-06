118163038b8766a268fdc99331b9c15891623f1d
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-02-17 17:58:37 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,7 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+const vegies = ingredients.map(ingredient => `<li class="item">${ingredient}</li>`).join("");
+list.innerHTML = vegies;
